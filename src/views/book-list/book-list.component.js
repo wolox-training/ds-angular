99dbff97704df4e5aeaba0ca750ffafb2c4b6d3e
David Sandoval <david281199@gmail.com>
2021-03-25 11:59:00 -0500

    book list view updated with data json
    search bar added to view

--- src/views/book-list/book-list.component.js
+++ src/views/book-list/book-list.component.js
@@ -3,7 +3,16 @@ import bookCover from '../../assets/images/book-cover.png';
 
 angular.module('app-bootstrap').component('bookList', {
   template: require('./book-list.html'),
-  controller: [function () {
+  controller: ['bookService', function (bookService) {
     this.bookCover = bookCover;
+    this.books = bookService.getBooks();
+    this.filter = '';
   }]
+}).filter('searchBook', function () {
+  return (books, filter) =>
+    books.filter((book) => {
+      const title = book.title.toLowerCase();
+      const author = book.author.toLowerCase();
+      return title.includes(filter) || author.includes(filter);
+    });
 });
