6c278f4046ce2ca6edae5c43bc6b65537ccf5f01
David Sandoval <david281199@gmail.com>
2021-03-29 13:57:10 -0500

    logout functionality added

--- src/components/navbar/navbar.component.js
+++ src/components/navbar/navbar.component.js
@@ -3,7 +3,13 @@ import logoWolox from '../../assets/images/LogoWolox.png';
 
 angular.module('app-bootstrap').component('navbar', {
   template: require('./navbar.html'),
-  controller: [function () {
-    this.logoWolox = logoWolox;
-  }]
+  controller: ['$state', 'localStorageService',
+    function ($state, localStorageService) {
+      this.logoWolox = logoWolox;
+
+      this.logout = () => {
+        localStorageService.clearAll();
+        $state.go('login');
+      };
+    }]
 });
